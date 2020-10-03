ade4c4d63cf872713e06c44a88deba16a28748ef
naoki85 <naoki.yoneyama.85@gmail.com>
2020-10-03 15:33:34 +0900

    eloquent relationships between User and Task

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get all of the tasks for the user.
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
