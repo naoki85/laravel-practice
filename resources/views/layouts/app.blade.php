75a1054d901e0dc5a9d3005e2c02c5653aebc311
naoki85 <naoki.yoneyama.85@gmail.com>
2020-10-03 10:31:59 +0900

    Add bootstrap css and js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,16 +3,40 @@
     <head>
         <title>Laravel Quickstart - Basic</title>
 
-        <!-- CSS And JavaScript -->
+        <!-- Fonts -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+        <!-- Styles -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Lato';
+            }
+            .fa-btn {
+                margin-right: 6px;
+            }
+        </style>
     </head>
 
     <body>
-        <div class="container">
-            <nav class="navbar navbar-default">
-                <!-- Navbar Contents -->
-            </nav>
-        </div>
-
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+    
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        Task List
+                    </a>
+                </div>
+    
+            </div>
+        </nav>
+        
         @yield('content')
+
+        <!-- JavaScripts -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </body>
 </html>
