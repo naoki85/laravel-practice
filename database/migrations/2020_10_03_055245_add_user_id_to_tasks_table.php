4cdaae415e26e040828b256c1c2dd5bd0fbea6cc
naoki85 <naoki.yoneyama.85@gmail.com>
2020-10-03 15:06:03 +0900

    Add user_id column to tasks

---
+++ database/migrations/2020_10_03_055245_add_user_id_to_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->integer('user_id')->default(0)->after('id')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
