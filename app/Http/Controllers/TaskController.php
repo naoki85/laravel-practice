d1b56f83ed3065f80c48b6c8d97a99f35dc419f2
naoki85 <naoki.yoneyama.85@gmail.com>
2020-10-03 18:52:54 +0900

    Create TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+
+    /**
+     * Display a list of all of the user's task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+    
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
+    }
+    
+    /**
+     * Create a new task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+    
+        return redirect('/');
+    }
+    
+    /**
+     * Destroy the given task.
+     *
+     * @param  Request  $request
+     * @param  string  $taskId
+     * @return Response
+     */
+    public function destroy(Request $request, $taskId)
+    {
+        Task::findOrFail($taskId)->delete();
+    
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,49 +11,12 @@
 |
 */
 
-use App\Task;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 
-/**
- * Display All Tasks
- */
-Route::get('/', function () {
-    $tasks = Task::orderBy('created_at', 'asc')->get();
-
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
-
-/**
- * Add A New Task
- */
-Route::post('/task', function(Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-    
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
-/**
- * Delete An Existing Task
- */
-Route::delete('/task/{id}', function($id) {
-    Task::findOrFail($id)->delete();
-
-    return redirect('/');
-});
+Route::get('/', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{taskId}', 'TaskController@destroy');
 
 // Authentication Routes...
 Route::get('/login', 'Auth\LoginController@showLoginForm');
