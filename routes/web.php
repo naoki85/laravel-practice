0bdda37e98504260525b7eb98e171c3dc20c5e9f
naoki85 <naoki.yoneyama.85@gmail.com>
2020-10-03 10:36:06 +0900

    Add the ability to create new tasks

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,12 @@ Route::post('/task', function(Request $request) {
             ->withInput()
             ->withErrors($validator);
     }
-    //
+    
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/tasks');
 });
 
 /**
